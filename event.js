6aaf5660df0956ef9fd0a92715fecd6e12d46ba8
Priti Solanki <pritisolanki@ME-13-NTB.kontent.ai>
2022-12-12 16:55:55 -0500

    Setup Agenda

--- event.js
+++ event.js
@@ -22,6 +22,7 @@ const createElement = (elementType, classToAdd, attribute, attributeValue) => {
 deliveryClient
   .items()
   .type('event')
+  .depthParameter(3)
   .toPromise()
   .then(response => {
     console.log(response)
@@ -35,9 +36,47 @@ deliveryClient
         }
       }
     }
+    const getSpeaker = (speakerName) =>{
+      return ({
+        "name": response.data.linkedItems[speakerName].system.name,
+        "company" : response.data.linkedItems[speakerName].elements.company.value,
+        "bio" : response.data.linkedItems[speakerName].elements.bio.value,
+        "photo" : response.data.linkedItems[speakerName].elements.photo.value[0].url,
+        "designation" : response.data.linkedItems[speakerName].elements.title.value,
+      })
+    }
+    const fetchSessionItem = (sessionName) =>{
+      
+      for(row of linkedItems){
+        if(row.system.type == 'session_list' && row.system.codename == sessionName){
+          
+          const sessionTopic = row.elements.topic.linkedItems[0].elements.name.value
+          const sessionTopicDtl = row.elements.topic.linkedItems[0].elements.topic_details.value
+          const sessionSpeakerKey = row.elements.speaker.value
+            
+          let speakerList = []
+          for(speaker of sessionSpeakerKey){
+            spk = getSpeaker(speaker)
+            speakerList.push(spk)
+          }
+          const sessionroom = row.elements.room.value[0]
+          const sessionStartDate= row.elements.date_n_time.value
+          const sessionEndDate = row.elements.end_date___time.value
+          return({
+            "topic":sessionTopic,
+            "topic_details": sessionTopicDtl,
+            "speaker": speakerList,
+            "room": sessionroom,
+            "start":sessionStartDate,
+            "end":sessionEndDate
+          })
+        }
+      }
+    }
+
 
     const fetchSponsor = (sponsorItem) =>{
-      return (`<p><a href="${response.data.linkedItems[sponsorItem].elements.url.value}"><img src="${response.data.linkedItems[sponsorItem].elements.logo.value[0].url}" ></a></p>`)
+      return (`<p align="center"><a href="${response.data.linkedItems[sponsorItem].elements.url.value}"><img src="${response.data.linkedItems[sponsorItem].elements.logo.value[0].url}" width="200" height="200"></a></p>`)
     }
     const richTextElement = response.data.items[0].elements.intro_message;
     const resolvedRichTextObject = window['kontentDelivery'].createRichTextHtmlResolver().resolveRichText({
@@ -59,8 +98,6 @@ deliveryClient
     const locationData = response.data.linkedItems.venue_details.elements.location.value;
     const locationInfoElement = document.getElementById('locationInfo')
     locationInfoElement.innerText = locationData
-
-    
     
     const sponsorList = response.data.items[0].elements.sponsor.value;
     let i = 1
@@ -69,7 +106,6 @@ deliveryClient
       sDetails = fetchSponsor(sponsor)
       keyC = `sponsor${i}`
       const sponsorElem = document.getElementById(keyC);
-      console.log(sponsorElem)
       sponsorElem.innerHTML=sDetails
       i = i + 1
     }
@@ -88,21 +124,47 @@ deliveryClient
         app.appendChild(introElement)
       }else if(page == 'agenda.html'){
         //agenda
-        const agendaDetails = document.getElementById('agendaDetails')
+        const agendaDetailsEle = document.getElementById('agendaDetails')
         const introPhoto = item.elements.photo.value[0].url
         const SectionImgElement = createElement('img','row','src',introPhoto)
         SectionImgElement.setAttribute('align','right')
-        agendaDetails.appendChild(SectionImgElement)
-      }else{
+        agendaDetailsEle.appendChild(SectionImgElement)
+
+        const agendaDay = response.data.linkedItems.full_agenda___denver_conf.elements.day.value
+        const agendaDetails = response.data.linkedItems.full_agenda___denver_conf.elements.sessions.value
+        const sessionList = [];
+        
+        for(row of agendaDetails){
+          sessionlist = fetchSessionItem(row)
+          const startTime = new Date(sessionlist['start']).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+          const endTime =new Date(sessionlist['end']).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  
+          let sessionRow = `
+          <h3>${sessionlist['topic']}</h3>
+          <h5>${startTime} - ${endTime} @${sessionlist['room']}</h5>
+          <div style="padding-left:20px">${sessionlist['topic_details']}</div>`;
+          console.log(sessionRow)
+          let spkitem = ''
+          for (item of sessionlist['speaker'])
+          {
+            spkitem = spkitem + `${item.name}`+ ' ';
+          }
+          
+          sessionRow = sessionRow + `<p>${spkitem} </p>`
+          const sessionEle = createElement('div','row','innerHTML',sessionRow)
+          agendaDetailsEle.appendChild(sessionEle)
+        }
+        
+      }
+      else{
         //venue
         const drivingDirection = item.elements.venue.linkedItems[0].elements.driving_directions.value;
         
-        const maps =item.elements.venue.linkedItems[0].elements.maps.value[0].url;
+        const maps = item.elements.venue.linkedItems[0].elements.maps.value[0].url;
         const venueName = item.elements.venue.linkedItems[0].elements.name.value;
         const publicTransport = item.elements.venue.linkedItems[0].elements.public_transportation.value;
         const venue_hero_image = item.elements.venue.linkedItems[0].elements.venue_hero_image.value[0].url;
       }
-      
       const regEmail = response.data.linkedItems.contact___registration___general_questions.elements.email.value;
       const regPhone = response.data.linkedItems.contact___registration___general_questions.elements.phone_number.value;
       const registrationInfoElement = document.getElementById('registrationEmail')
@@ -117,5 +179,12 @@ deliveryClient
       const sponsorPhoneElement = document.getElementById('sponsorPhone')
       sponsorPhoneElement.innerText = (sponsorPhone)
 
+      const fb = response.data.linkedItems.facebook.elements.url.value;
+      const facebookLinkElement = document.getElementById('facebookLink');
+      facebookLinkElement.setAttribute("href",fb);
+
+      const tw = response.data.linkedItems.twitter_dallas.elements.url.value;
+      const twitterElement = document.getElementById('twitterLink');
+      twitterElement.setAttribute("href",tw);
     });
   });  
