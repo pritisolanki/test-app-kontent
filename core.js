20f526d24abff516f8ee447f87822d073e2b0f10
Priti Solanki <pritisolanki@ME-13-NTB.kontent.ai>
2022-12-13 09:09:35 -0500

    Task: Remove unwanted code

--- core.js
+++ core.js
@@ -5,3 +5,11 @@ const Kk = window['kontentDelivery'];
 const deliveryClient = new Kk.createDeliveryClient({
   projectId: 'a6b9420d-8938-0096-4de3-50425aa9e2ea'
 });
+
+
+const addToElementbyId = (elementType, id, parent) => {
+  const element = document.createElement(elementType);
+  element.setAttribute('id', id);
+  parent.appendChild(element);
+  return element;
+};
